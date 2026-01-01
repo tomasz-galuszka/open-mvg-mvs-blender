7b070ab7e77baef39c30171ea14dcd217478933a
Tomasz-Galuszka-Allegro <100775897+atomaszg@users.noreply.github.com>
2026-01-01 19:42:18 +0100

    working version

--- app/openmvs/run.sh
+++ app/openmvs/run.sh
@@ -4,15 +4,15 @@ set -e
 echo -e "▶ OpenMVS processing started ... \n"
 
 # DensifyPointCloud – zagęszcza chmurę punktów z SfM
-DensifyPointCloud scene.mvs --resolution-level 2 --min-resolution 640 --max-resolution 1280 --max-threads 1
+DensifyPointCloud scene.mvs --resolution-level 2 --min-resolution 640 --max-resolution 1280 --max-threads 2
 
 # ReconstructMesh – buduje siatkę (mesh) na podstawie gęstej chmury punktów
-ReconstructMesh -i scene_dense.mvs --archive-type 2
+ReconstructMesh -i scene_dense.mvs --archive-type=2
 
 # TextureMesh – nakłada tekstury na siatkę (UV mapping + kolor)
-TextureMesh scene_dense_mesh.mvs --archive-type 2
+TextureMesh scene_dense_mesh.mvs --archive-type=2
 
-# Ekspport do blendera
+# Ekspport do blendera // nie działa
 TransformScene \
   --input-file=scene_dense_mesh_texture.mvs \
   --mesh-file=scene_dense_mesh_texture.ply \

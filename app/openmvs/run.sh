248c621fa6a0934cf680e38cefa09202b230ed01
Tomasz-Galuszka-Allegro <100775897+atomaszg@users.noreply.github.com>
2026-01-06 18:08:02 +0100

    Fix rotation - proba druga

--- app/openmvs/run.sh
+++ app/openmvs/run.sh
@@ -3,16 +3,20 @@ set -e
 
 echo -e "▶ OpenMVS processing started ... \n"
 
-# DensifyPointCloud – zagęszcza chmurę punktów z SfM
+echo -e "--- DensifyPointCloud – zagęszcza chmurę punktów z SfM ---\n"
 DensifyPointCloud scene.mvs --max-threads 4
 
-# ReconstructMesh – buduje siatkę (mesh) na podstawie gęstej chmury punktów
+echo -e "--- ReconstructMesh – buduje siatkę (mesh) na podstawie gęstej chmury punktów ---\n"
 ReconstructMesh -i scene_dense.mvs --archive-type=2
 
-# Popraw normalne jeśli mesh jest odwrócony
-RefineMesh scene_dense_mesh.mvs --fix-normals --fix-scene
+echo -e "--- RefineMesh Popraw normalne jeśli mesh jest odwrócony ---\n"
+RefineMesh -i scene_dense_mesh.mvs \
+  --ensure-edge-size 1 \
+  --close-holes 30 \
+  --decimate 0.3 \
+  -o scene_refined.mvs
 
-# TextureMesh – nakłada tekstury na siatkę (UV mapping + kolor)
-TextureMesh scene_dense_mesh.mvs --archive-type=2 --export-type OBJ
+echo -e "--- TextureMesh – nakłada tekstury na siatkę (UV mapping + kolor) ---\n"
+TextureMesh scene_refined.mvs --archive-type=2 --export-type OBJ
 
 echo -e "▶ OpenMVS processing finished!"
